ecb5aba30da7f8d8a896c222a95e3bffbe0bff89
Carolina Bis <carolbis09@gmail.com>
2025-11-03 16:49:16 -0300

    initialize project with React, Vite, and ESLint

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Banner from "./componentes/Banner/banner";
+import Formulario from "./componentes/Formulario/Formulario";
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
